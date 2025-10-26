c87f6e22f99a3fa6048ef9e7f01a9b520a55c0bc
Joseph Micallef <micallefje@gmail.com>
2025-10-26 11:51:39 +0000

    edit wording in verification commands

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -26,9 +26,9 @@ resource "terraform_data" "verify_output" {
     command = <<EOF
       sleep 120
       if aws s3 ls s3://${aws_s3_bucket.ec2_bucket.bucket}/output/; then
-        echo "Success! Output files found in S3:"
+        echo "Success! Output file found in S3!"
       else
-        echo "Operation failed. No output files found in S3."
+        echo "Operation failed. No output file found in S3."
       fi
     EOF
   }
