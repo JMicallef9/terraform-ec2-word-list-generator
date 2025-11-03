e02f49718917a4611302c13daa9b631191b3ca96
Joseph Micallef <micallefje@gmail.com>
2025-11-03 15:36:05 +0000

    add aws lambda permission

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,3 +6,11 @@ resource "aws_lambda_function" "output_notifier" {
 
   filename = "${path.module}/lambda/output_notifier.zip"
 }
+
+resource "aws_lambda_permission" "allow_s3_trigger" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.output_notifier.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.ec2_bucket.arn
+}
