53d6ce4b4f3701ff3e859f397887aea78d3212af
Joseph Micallef <micallefje@gmail.com>
2025-11-03 12:11:11 +0000

    add new lambda role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -42,3 +42,20 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_s3_event_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
