5427447cbe70a316bde955b077bcb638c29b4724
Joseph Micallef <micallefje@gmail.com>
2025-11-04 11:38:11 +0000

    amend lambda details

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,10 @@
 resource "aws_lambda_function" "output_notifier" {
   function_name = "wordlist-output-notifier"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "script.lambda_handler"
+  handler       = "output_notifier.lambda_handler"
   runtime       = "python3.12"
 
-  filename = "${path.module}/lambda/output_notifier.zip"
+  filename = "${path.module}/lambda.zip"
 }
 
 resource "aws_lambda_permission" "allow_s3_trigger" {
