741d6b130b59b4542d1dbd28b40f1fc3a5ce0dd3
Joseph Micallef <micallefje@gmail.com>
2025-11-04 10:27:51 +0000

    add s3 bucket notification

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -14,3 +14,15 @@ resource "aws_lambda_permission" "allow_s3_trigger" {
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.ec2_bucket.arn
 }
+
+resource "aws_s3_bucket_notification" "output_trigger" {
+  bucket = aws_s3_bucket.ec2_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.output_notifier.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "output/"
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3_trigger]
+}
