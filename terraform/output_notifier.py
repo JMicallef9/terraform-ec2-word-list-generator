d609b055acc121aba4fb61924856402fc0f1e6b8
Joseph Micallef <micallefje@gmail.com>
2025-11-04 11:37:44 +0000

    add Python file for lambda

---
+++ terraform/output_notifier.py
@@ -0,0 +1,6 @@
+import json
+
+def lambda_handler(event, context):
+    print("New object uploaded to output/:")
+    print(json.dumps(event, indent=2))
+    return {"statusCode": 200, "body": "Notification received"}
