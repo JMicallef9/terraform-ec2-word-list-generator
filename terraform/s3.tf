0cc22dab9707a0d8aa6fef1540a81008acd4d120
Joseph Micallef <micallefje@gmail.com>
2025-11-08 11:58:30 +0000

    remove local exec from s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,16 +5,3 @@ resource "aws_s3_bucket" "ec2_bucket" {
     Name = "ec2_bucket"
   }
 }
-
-resource "terraform_data" "upload_input" {
-  provisioner "local-exec" {
-    command = "${path.module}/docker/venv/bin/python ${path.module}/script/s3_upload.py"
-    environment = {
-      BUCKET_NAME = var.bucket_name
-      INPUT_KEY = var.input_key
-      LOCAL_FILE = var.local_filepath
-    }
-  }
-
-  depends_on = [aws_s3_bucket.ec2_bucket]
-}
