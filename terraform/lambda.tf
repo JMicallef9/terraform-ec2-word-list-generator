9f49b78ffeb4f2a983ff2f0383221a8ab8ab17c4
Joseph Micallef <micallefje@gmail.com>
2025-11-09 12:39:47 +0000

    modify lambda for input trigger

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,7 @@
-resource "aws_lambda_function" "output_notifier" {
-  function_name = "wordlist-output-notifier"
+resource "aws_lambda_function" "send_ssm" {
+  function_name = "send_ssm"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "output_notifier.lambda_handler"
+  handler       = "send_ssm.lambda_handler"
   runtime       = "python3.12"
 
   filename = "${path.module}/lambda.zip"
@@ -12,18 +12,18 @@ resource "aws_lambda_function" "output_notifier" {
 resource "aws_lambda_permission" "allow_s3_trigger" {
   statement_id  = "AllowExecutionFromS3"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.output_notifier.function_name
+  function_name = aws_lambda_function.send_ssm.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.ec2_bucket.arn
 }
 
-resource "aws_s3_bucket_notification" "output_trigger" {
+resource "aws_s3_bucket_notification" "input_trigger" {
   bucket = aws_s3_bucket.ec2_bucket.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.output_notifier.arn
+    lambda_function_arn = aws_lambda_function.send_ssm.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "output/"
+    filter_prefix       = "input/"
   }
 
   depends_on = [aws_lambda_permission.allow_s3_trigger]
