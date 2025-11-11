5385d0fd84e6d2068f4187f7680286078bea4eef
Joseph Micallef <micallefje@gmail.com>
2025-11-11 11:34:38 +0000

    update Docker command with environment variables

--- terraform/send_ssm.py
+++ terraform/send_ssm.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
 
     ec2_instance_id = os.environ['EC2_INSTANCE_ID']
 
-    command = f"docker run jmicallef9/word-list-generator-ec2:latest process s3://{bucket}{key}" # use process?
+    command = f"docker run -e BUCKET_NAME={bucket} -e INPUT_KEY={key} jmicallef9/word-list-generator-ec2:latest"
 
     response = ssm_client.send_command(
         InstanceIds=[ec2_instance_id],
